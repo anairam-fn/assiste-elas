42aecedf90e73624e7cedfa39ccf40a52372a19d
Mariana Nunes <marinun47@gmail.com>
2022-07-20 01:08:30 -0300

    :sparkles: add new matches routes

--- src/controllers/matchesController.js
+++ src/controllers/matchesController.js
@@ -52,22 +52,31 @@ const getAllMatches = async (req, res) => {
   }
 };
 
-// GET/matches/team -> lista partidas de um time específico
+// GET/matches/team -> lista partidas de um time específico (escolhe seleção ou clube)
 
 const getMatchByTeam = async (req, res) => {
   try {
-    const { name } = req.query;
+    const { name, type } = req.query;
 
     const matchesPopulated = await MatchSchema.find()
       .populate("team1")
       .populate("team2");
 
-    const teamFound = await matchesPopulated.filter(
-      (match) => match.team1.name === name || match.team2.name === name
+    const typeFound = await matchesPopulated.filter(
+      (match) =>
+        match.team1.type.toLowerCase() === type ||
+        match.team2.type.toLowerCase() === type
+    );
+    console.log(typeFound);
+
+    const teamFound = await typeFound.filter(
+      (match) =>
+        match.team1.name.toLowerCase() === name ||
+        match.team2.name.toLowerCase() === name
     );
 
-    if(!teamFound[0]) {
-        return res.status(404).json({ message: "Match not found!"})
+    if (!teamFound[0]) {
+      return res.status(404).json({ message: "Match not found!" });
     }
 
     res.status(200).json(teamFound);
@@ -77,11 +86,50 @@ const getMatchByTeam = async (req, res) => {
   }
 };
 
-// GET/matches/nationalteam -> lista partidas de uma seleção específica
-
 // GET/matches/day -> lista partidas do dia
 
+const matchesByDay = async (req, res) => {
+  try {
+    const { date } = req.query
+
+    const match = await MatchSchema.find({ date: date })
+
+    res.status(200).json(match)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: error.message })
+  }
+}
+
 // PATCH/match/:id -> atualiza dados de uma partida
+
+const updateMatch = async (req, res) => {
+  try {
+    const { team1Id, team2Id, local, date, time, type, streaming } = req.body;
+    const { id } = req.params
+
+    const match = await MatchSchema.findByIdAndUpdate(id, {
+      team1Id,
+      team2Id,
+      local,
+      date,
+      time,
+      type,
+      streaming,
+    });
+
+    const updatedMatch = await MatchSchema.findById(req.params.id)
+      .populate("team1")
+      .populate("team2");
+
+    //const message = `${match} updated to ${updatedMatch}`;
+
+    res.status(200).json(updatedMatch);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // DELETE/match/:id -> exclui partida por id (excluir partida por data? serase)
 
@@ -104,5 +152,7 @@ module.exports = {
   createMatch,
   getAllMatches,
   getMatchByTeam,
+  matchesByDay,
+  updateMatch,
   deleteMatch,
 };
--- src/routes/matchesRoutes.js
+++ src/routes/matchesRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post("/match", controller.createMatch);
 router.get("/matches", controller.getAllMatches)
 router.get("/matches/team", controller.getMatchByTeam)
+router.get("/matches/day", controller.matchesByDay)
+router.patch("/match/:id", controller.updateMatch)
 router.delete("/match/:id", controller.deleteMatch)
 
 module.exports = router;
